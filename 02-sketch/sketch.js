c9c64a081d6e53eace1a3adfd5a079368f2b22df
Aya A. <ayaalsabahi735@gmail.com>
2025-02-23 22:42:39 +0300

    finalized

--- 02-sketch/sketch.js
+++ 02-sketch/sketch.js
@@ -1,14 +1,14 @@
 let pastelColors = [
-  [255, 209, 186], // Peach
-  [248, 200, 220], // Blush Pink
-  [214, 195, 252], // Lavender
-  [179, 229, 252]  // Powder Blue
+  [255, 209, 186], 
+  [248, 200, 220], 
+  [214, 195, 252], 
+  [179, 229, 252] 
 ];
 
 let canvasSize = 400;
 let currentBlocks = [];
-let blockSpeed = 7;
-let maxDist = 20; 
+let blockSpeed = 15;
+let maxDist = 50; 
 
 function setup() {
   createCanvas(canvasSize, canvasSize);
@@ -38,6 +38,8 @@ function computeblock(){
       h: randomHeight, 
       w: randomWidth,
       d: 0, 
+      max: floor(random(10,maxDist)),
+      s: floor(random(3,blockSpeed)),
       forward: true
     };
     currentBlocks.push(block);
@@ -45,34 +47,40 @@ function computeblock(){
   
 }
 function displayblocks(){
-  // Update and display all blocks
   for (let block of currentBlocks) {
-
-    if(block.d > maxDist){
+    //clearing the first drawn block
+    if(block.d > block.max){
       block.forward = false;
-      fill(220); //fill the current block spot
+      stroke(220);
+      fill(220);
+      strokeWeight(1.2);
       rect(block.x, block.y, block.h, block.w);
       block.d -=1; 
     }
 
+    //if going fowrads
     if(block.forward){
       block.angle += block.rotationSpeed; 
-      block.x += cos(block.angle) * blockSpeed;
-      block.y += sin(block.angle) * blockSpeed;
+      block.x += cos(block.angle) * block.s;
+      block.y += sin(block.angle) * block.s;
       block.d +=1;
       fill(block.color);
-      stroke(0.5);
+      strokeWeight(0.5);
+      stroke(0);
       rect(block.x, block.y, block.h, block.w);
-      
     }
-    else if (!block.forward && block.d >= 0){
-      fill(220);
-      noStroke();
-      rect(block.x, block.y, block.h, block.w);
 
+    //if going backwards
+    else if (!block.forward && block.d >= 0){
+      stroke(220);
+      fill(220);
+      strokeWeight(1.2);
+      let offsetX = cos(block.angle) * block.s;
+      let offsetY = sin(block.angle) * block.s;
+      rect(block.x - offsetX, block.y - offsetY, block.h, block.w);
+      block.x -= cos(block.angle) * block.s;
+      block.y -= sin(block.angle) * block.s;
       block.angle -= block.rotationSpeed; 
-      block.x -= cos(block.angle) * blockSpeed;
-      block.y -= sin(block.angle) * blockSpeed;
       block.d -=1;
     }
   
