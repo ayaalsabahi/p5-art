958665570fa1ed7fd805483fc7b62f1b1e54f0c8
Aya A. <ayaalsabahi735@gmail.com>
2025-02-25 16:29:48 +0300

    added molds + some randomness

--- 03-sketch/Mold.js
+++ 03-sketch/Mold.js
@@ -1,9 +1,9 @@
 class Mold{
     constructor(){
         this.r = 10; //radius of the mold
-        this.x = width/2; 
-        this.y = width/2; 
-        this.heading = 0; //angle to where it is heading in
+        this.x = random(width);
+        this.y = random(height);
+        this.heading = random(360); //angle to where it is heading in
         this.rotAngle = 45; //rotation angle of the thingy
         this.vx = cos(this.heading);
         this.vy = sin(this.heading ); 
@@ -28,6 +28,12 @@ class Mold{
     }
 
     update(){
+        this.vx = cos(this.heading);
+        this.vy = sin(this.heading); 
+
+        this.x = this.x + this.vx; 
+        this.y = this.y + this.vy; 
+
         this.rSensorPos.x = this.x + this.sensorDistance*cos(this.heading + this.sensorAngle); //+ sensor angle
         this.rSensorPos.y = this.y + this.sensorDistance*sin(this.heading + this.sensorAngle);
         
@@ -46,7 +52,6 @@ class Mold{
         index = 4*(d * floor(this.mSensorPos.y)) * (d * width) + 4*(d * floor(this.mSensorPos.x)); 
         m = pixels[index]; 
         
-        print(r,l,m);
 
         //here we will check conditions of where to head to 
         if(m > l && m > l){
--- 03-sketch/sketch.js
+++ 03-sketch/sketch.js
@@ -2,18 +2,26 @@
 
 let m; 
 let d; 
+let molds = []; 
+let numMolds = 5; 
 
 function setup() {
   createCanvas(400, 400);
   angleMode(DEGREES); 
   d =  pixelDensity();
-  m = new Mold();
+
+  //creating the molds
+  for(let i = 0; i < numMolds; i++){
+    append(molds, new Mold()); 
+  }
   
 }
 
 function draw() {
   background(220);
   loadPixels();
-  m.update();
-  m.display();   
+  for(let i = 0; i < numMolds; i++){
+    molds[i].update();
+    molds[i].display();
+  }
 }
